f395f33ec6773315a0f9a4366641644ea1a1578a
Subhashish Nabajja <subhashishnabajja1575643@gmail.com>
2024-12-05 15:49:34 +0530

    Update client.ts

--- client/src/controller/client.ts
+++ client/src/controller/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://rembg-api.snabajja.in",
+  baseURL: "https://rembg-api.snabajja.in",
 });
 
 export default axiosClient;
